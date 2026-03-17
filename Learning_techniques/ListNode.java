c230e065b11a12e1aad509625cbc1c820a332d24
MATRUNI <h.uyadav2004@gmail.com>
2026-03-17 22:02:47 +0530

    Problem: 237

---
+++ Learning_techniques/ListNode.java
@@ -0,0 +1,13 @@
+// Problem 237
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+}
